de940714864ef4f9c8b674fc1261a21bddf39550
Harman14430 <sin14430@sheridancollege.ca>
2024-10-07 13:29:10 -0400

    Update Project_S,W&G.py

--- Project_S,W&G.py
+++ Project_S,W&G.py
@@ -1,21 +1,22 @@
-computer=-1
-youstr =input("Enter your choice:")
-youDict={"s":1,"w":-1,"g":0}
-you=youDict[youstr]
-if(computer== you):
-     print("draw")
+import random
 
-if(computer ==-1 and you==1 ):
+computer = random.randint(-1, 1)
+youstr = input("Enter your choice:")
+youDict = {"s": 1, "w": -1, "g": 0}
+reverseDict={1:"Snake",-1:"Water",0:"Gun"}
+
+you = youDict[youstr]
+print(f"you choose {reverseDict[you]}\ncomputer choose {reverseDict[computer]}")
+
+if (computer == -1 and you == 1):
     print("you wins")
-elif(computer ==-1 and you ==0):
+elif (computer == -1 and you == 0):
     print("you loose")
-if(computer ==1 and you==-1 ):
+if (computer == 1 and you == -1):
     print("you loose")
-elif(computer ==1 and you==0 ):
+elif (computer == 1 and you == 0):
     print("you wins")
-if(computer ==0 and you==1 ):
+if (computer == 0 and you == 1):
     print("you loose")
-elif(computer ==0 and you==-1 ):
+elif (computer == 0 and you == -1):
     print("you wins")
-else:
-    print("something went wrong")
